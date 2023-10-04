243416b3aa02a2943de34a9cb9bf722b8017021c
marti <marti@smartwash.dhcp.wustl.edu>
2023-10-04 14:08:36 -0500

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,15 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
 		
+		StdDraw.setPenRadius(0.05);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(11,10, 9, 2);
+		StdDraw.setPenColor(Color.BLACK);
+		
+		StdDraw.rectangle(11,8, 9, 4);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,53 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo= in.nextDouble();
+		double parameterThree = in.nextDouble();
+		
+		StdDraw.setPenRadius(0.05);
+		
+		if(shapeType.equals("ellipse"))
+		{
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if(isFilled)
+			{
+				StdDraw.filledEllipse(0.5, 0.5, parameterOne, parameterThree);
+			}
+			else
+				StdDraw.ellipse(0.5, 0.5, parameterOne, parameterThree);
+		}
+		else if(shapeType.equals("rectangle"))
+		{
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if(isFilled)
+			{
+				StdDraw.filledRectangle(0.5, 0.5, parameterOne, parameterThree);
+			}
+			else
+				StdDraw.rectangle(0.5, 0.5, parameterOne, parameterThree);
+		}
+		else if(shapeType.equals("triangle"))
+		{
+			double parameterFour = in.nextDouble();
+			double parameterFive = in.nextDouble();
+			double parameterSix = in.nextDouble();
+			 double[] triX  = {parameterOne, parameterThree, parameterFive};
+			 double[] triY = {parameterTwo, parameterFour, parameterSix};
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if(!isFilled)
+			{
+				StdDraw.line(parameterOne, parameterTwo, parameterThree, parameterFour);
+				StdDraw.line(parameterThree, parameterFour, parameterFive, parameterSix );
+				StdDraw.line(parameterFive, parameterSix, parameterOne, parameterTwo);
+			}
+			else
+				StdDraw.filledPolygon(triX,triY);
+		}
 	}
 }
